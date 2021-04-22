3bffddebe9c3bc4518c9d7ffa3193863b7cc55aa
Faisal <asdfaisal39@gmail.com>
2021-04-22 01:43:05 -0700

    amplify

--- App.tsx
+++ App.tsx
@@ -5,12 +5,14 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+//import { withAuthenticator } from 'aws-amplify-react-native'
+import { withAuthenticator } from '@aws-amplify/ui-react'; //authetication done with this only
 
 import Amplify from 'aws-amplify' //setup for cloud
 import config from './src/aws-exports'
 Amplify.configure(config)
 
-export default function App() {
+ function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
@@ -25,3 +27,4 @@ export default function App() {
     );
   }
 }
+export default withAuthenticator(App)
